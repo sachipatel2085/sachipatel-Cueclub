6c8fc50be52d0805eafea0b4666fc85b805055af
sachipatel <sachixl300@gmail.com>
2026-08-05 23:30:20 +0530

    aa

--- src/components/testimonials/testimonialsData.js
+++ src/components/testimonials/testimonialsData.js
@@ -4,7 +4,7 @@ const testimonials = [
     name: "James Carter",
     role: "League Player",
     rating: 5,
-    image: "/images/testimonials/person1.jpg",
+    image: "./testimonials/person1.jpg",
     review:
       "Absolutely the best cue sports club in Coventry. Premium tables, fantastic atmosphere and professional staff.",
   },
@@ -13,7 +13,7 @@ const testimonials = [
     name: "Sarah Williams",
     role: "Member",
     rating: 5,
-    image: "/images/testimonials/person2.jpg",
+    image: "./testimonials/person2.jpg",
     review:
       "Beautiful venue with excellent lighting and well-maintained tables. The membership is worth every penny.",
   },
@@ -22,7 +22,7 @@ const testimonials = [
     name: "Michael Brown",
     role: "Tournament Player",
     rating: 5,
-    image: "/images/testimonials/person3.jpg",
+    image: "./testimonials/person3.jpg",
     review:
       "I've played at many clubs across the UK and Cue Club Coventry is easily one of my favourites.",
   },
